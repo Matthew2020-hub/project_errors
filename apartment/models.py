3548adc463de0c9bcba55e58b9bb195f4caf959e
Matthew <akinolatolulope24@gmail.com>
2022-01-15 23:02:19 +0100

    UUID error handling

---
+++ apartment/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Apartment(models.Model):
+    # id = models.IntegerField(primary_key=True, unique=True)
+    apartment_id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
+    name = models.CharField(max_length=40, null=True)
+    category = models.CharField(max_length=50, null=True)
+    # image = models.ImageField()
+    price = models.CharField(max_length=50, null=True)
+    location = models.CharField(max_length=30, null=True)
+    agent = models.CharField(max_length=30, null=True)
+
+    class Meta:
+        ordering = ['category']
